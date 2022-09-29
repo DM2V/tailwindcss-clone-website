138cbbfa01338e629e52ed99b16439f1b46415f2
DM2V <marcelo.malte11@gmail.com>
2022-09-29 18:02:21 -0500

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
         sans: "'Work Sans', sans-serif",
         cascadia: '"CascadiaCode"',
       },
+      colors: {
+        "do-blue-dark": "#081b4b",
+        "do-blue-medium": "rgb(0, 44, 155)",
+        "do-blue-light": "rgb(0, 105, 255)",
+      },
     },
   },
   plugins: [],

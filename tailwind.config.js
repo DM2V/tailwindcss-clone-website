5a20b77aa9b29fe6545607c03c38f0a07c662a29
DM2V <marcelo.malte11@gmail.com>
2022-10-03 21:56:25 -0500

    add navbar 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,21 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: "'Work Sans', sans-serif",
+        inter: "'Inter', sans-serif",
         cascadia: '"CascadiaCode"',
       },
       colors: {
         "do-blue-dark": "#081b4b",
+        "do-blue-dark2": "#3c4a6e",
+        "do-blue-dark3": "rgb(36, 51, 90)",
+        "do-blue-dark4": "rgb(0, 44, 155)",
         "do-blue-medium": "rgb(0, 44, 155)",
         "do-blue-light": "rgb(0, 105, 255)",
+        "do-hover-navbar2": "rgb(227, 232, 244)",
+        "do-white2": "#f9fafe",
+      },
+      padding: {
+        75: "19rem",
       },
     },
   },
